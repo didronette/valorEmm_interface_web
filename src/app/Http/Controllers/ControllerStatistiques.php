50e5da409554549528f98396b070a9227fc95cf7
Zoé STAUDER <zoe.stauder@telecomnancy.eu>
2020-08-06 10:26:54 +0200

    tentative 13

--- src/app/Http/Controllers/ControllerStatistiques.php
+++ src/app/Http/Controllers/ControllerStatistiques.php
@@ -195,12 +195,13 @@ class ControllerStatistiques extends Controller
 
       $pdf->addPage($render);
 
-      $pdf->setOptions(['javascript-delay' => 3000,
-      'enable-javascript' => true
+      $pdf->setOptions(['javascript-delay' => 3000]);
+      $pdf->setOptions(['enable-javascript' => true]);
+      //',
       //'images' => true,
       //'enable-smart-shrinking' => true,
       //'no-stop-slow-scripts' => true
-      ]);
+      
  
       if (!$pdf->saveAs(public_path('report.pdf'))) {
         return $pdf->getError();
