624ba3e132b5b307b2b2d8c33a804bfcaad1f796
Zoé STAUDER <zoe.stauder@telecomnancy.eu>
2021-01-02 23:02:56 +0100

    Correction du bug à l'affichage des choix pour les nouvelles commandes v2

--- src/resources/views/saisie/nouvelleCommande/nouvelleCommandeDDS.blade.php
+++ src/resources/views/saisie/nouvelleCommande/nouvelleCommandeDDS.blade.php
@@ -7,19 +7,19 @@
 
 {{ Form::label('multiplicite1', '1') }}
 {{ Form::radio('multiplicite', '1', false, array('id'=>'multiplicite1')) }}
+<!--
+{{ Form::label('multiplicite2', '2') }}
+{{ Form::radio('multiplicite', '2', false, array('id'=>'multiplicite2')) }}
 
-{{ -- Form::label('multiplicite2', '2') -- }}
-{{ -- Form::radio('multiplicite', '2', false, array('id'=>'multiplicite2')) -- }}
+{{ Form::label('multiplicite1', '3') }}
+{{ Form::radio('multiplicite', '3', false, array('id'=>'multiplicite3')) }}
 
-{{ -- Form::label('multiplicite1', '3') -- }}
-{{ -- Form::radio('multiplicite', '3', false, array('id'=>'multiplicite3')) -- }}
+{{ Form::label('multiplicite1', '4') }}
+{{ Form::radio('multiplicite', '4', false, array('id'=>'multiplicite4')) }}
 
-{{ -- Form::label('multiplicite1', '4') -- }}
-{{ -- Form::radio('multiplicite', '4', false, array('id'=>'multiplicite4')) -- }}
-
-{{ -- Form::label('multiplicite1', '5') -- }}
-{{ -- Form::radio('multiplicite', '5', false, array('id'=>'multiplicite5')) -- }}
-
+{{ Form::label('multiplicite1', '5') }}
+{{ Form::radio('multiplicite', '5', false, array('id'=>'multiplicite5')) }}
+-->
 </div>
 
 
