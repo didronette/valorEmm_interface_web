b1e0b8cd6905a6f712cf0c601634512419823bcc
Zoé Stauder <zoe.stauder@telecomnancy.eu>
2021-01-21 09:15:00 +0100

    Choix de la multiplicité de 1 uniquement

--- src/resources/views/saisie/nouvelleCommande/nouvelleCommandeBenne.blade.php
+++ src/resources/views/saisie/nouvelleCommande/nouvelleCommandeBenne.blade.php
@@ -7,12 +7,12 @@
 {{ Form::label('multiplicite1', '1') }}
 {{ Form::radio('multiplicite', '1', false, array('id'=>'multiplicite1')) }}
 
-<!--
+
 {{ Form::label('multiplicite2', '2') }}
 {{ Form::radio('multiplicite', '2', false, array('id'=>'multiplicite2')) }}
 
 {{ Form::label('multiplicite3', '3') }}
 {{ Form::radio('multiplicite', '3', false, array('id'=>'multiplicite3')) }}
--->
+
 </div>
 @endsection
