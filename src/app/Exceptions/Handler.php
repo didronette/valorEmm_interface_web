fe612a8ddc1dbb170df76f7612090028e7bffd45
Zoé Stauder <zoe.stauder@telecomnancy.eu>
2021-01-21 08:11:02 +0100

    Redirect back si le code PIN est mauvais debug 4

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -49,6 +49,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if($exception instanceof MauvaisCodePin){
+            session()->put('error', $exception->message());
             return back()->withErrors($exception->message());
         }
 
